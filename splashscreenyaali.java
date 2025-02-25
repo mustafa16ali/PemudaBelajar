118fe9f515ea1cb6e50c3e908ce993eb831680e7
mustafa16ali <121558158+mustafa16ali@users.noreply.github.com>
2025-02-25 10:35:28 +0700

    Create splashscreenyaali.java
    ukk

---
+++ splashscreenyaali.java
@@ -0,0 +1,25 @@
+package com.example.membuataplikasikalkulator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class splashscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent splash = new Intent(splashscreen.this, MainActivity.class);
+                startActivity(splash);
+                finish();
+            }
+        },3000);
+    }
+}
